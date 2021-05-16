e8d222a8723307ed556278c55cec22be8e8511e0
Car <cararr@tlen.pl>
2021-05-16 20:43:39 +0200

    App provides post schedule route now.

--- source/controllers/schedule/ScheduleController.ts
+++ source/controllers/schedule/ScheduleController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { getManager, getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
 import { ScheduleTable } from '../../entities/ScheduleTable';
 import { Station } from '../../entities/Station';
 import { dailySchedule } from '../../../typeDefs/types';
@@ -15,7 +15,7 @@ export class ScheduleController {
 			const date = req.query.date;
 			if (typeof date === 'string') {
 				const response = await getRepository(ScheduleTable).find({
-					relations: ['station', ...returnEmployeeRelations()],
+					relations: ['station', ...returnEmployeeCellNames()],
 					where: { date: date },
 				});
 
@@ -23,8 +23,7 @@ export class ScheduleController {
 
 				// if there are no schedule for given data controller should send back null arrays for each station
 				if (!response.length) {
-					const stations = await getRepository(Station).find();
-					stations.forEach((station) => {
+					req.body.stations.forEach((station: Station) => {
 						completeDailySchedule[date][station.name] = new Array(
 							station.numberOfCellsInTable
 						).fill(null);
@@ -65,27 +64,95 @@ export class ScheduleController {
 		res: Response,
 		next: NextFunction
 	) => {
-		const scheduleRepository = await getRepository(ScheduleTable);
-		const test = await scheduleRepository.find({
-			relations: ['station', ...returnEmployeeRelations()],
-			where: { date: '2021-05-17', station: { id: 2 } },
+		const scheduleRepository = getRepository(ScheduleTable);
+		const schedulesToSave: ScheduleTable[] = [];
+
+		for (const stationName in req.body.schedules) {
+			if (
+				Object.prototype.hasOwnProperty.call(req.body.schedules, stationName)
+			) {
+				const stationEntity: Station = req.body.stations.find(
+					(stat: Station) => stat.name === stationName
+				);
+				const stationTable = req.body.schedules[stationName];
+				const employeeCells: { [cell: string]: Employee | null } = {};
+
+				returnEmployeeCellNames().forEach((cellName: string, index: number) => {
+					if (index >= stationEntity.numberOfCellsInTable) return;
+					const employee: Employee | null =
+						req.body.employees.find(
+							(emp: Employee) => emp.id === stationTable[index]?.id
+						) || null;
+					employeeCells[cellName] = employee;
+				});
+
+				const newSchedule = scheduleRepository.create({
+					date: req.body.date,
+					station: stationEntity,
+					...employeeCells,
+				});
+				schedulesToSave.push(newSchedule);
+			}
+		}
+		const response = await scheduleRepository.save(schedulesToSave);
+		res.send(response);
+	};
+
+	static loadStationsAndEmployees = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		try {
+			const stations = await getRepository(Station).find();
+			const employees = await getRepository(Employee).find();
+			req.body.stations = stations;
+			req.body.employees = employees;
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static reqBodyVeryfier = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		// Still need to check if arrays contains Employee or nulls...
+		const date = Object.keys(req.body)[0];
+		if (!validateDateString(date)) return res.status(400).send('Invalid date.');
+
+		const scheduleRepository = getRepository(ScheduleTable);
+		const currentSchedulesPerDate = await scheduleRepository.find({
+			relations: ['station', ...returnEmployeeCellNames()],
+			where: { date },
 		});
-		const newSchedule = await scheduleRepository.create({
-			date: '2021-05-17',
-			station: { id: 1 },
-			cell1Employee: {
-				firstName: 'A',
-				lastName: 'S',
-				id: 'f2c5c715-02fe-45fb-b621-67d746db26b4',
-			},
-		});
-		const response = await scheduleRepository.save(newSchedule);
-		console.log(test);
-		res.send(newSchedule);
+
+		if (currentSchedulesPerDate.length)
+			return res
+				.status(400)
+				.send('There is a schedule in the database for a given date.');
+
+		req.body.currentSchedulesPerDate = currentSchedulesPerDate;
+		req.body.date = date;
+		req.body.schedules = req.body[date];
+
+		for (const stationName of Object.keys(req.body.schedules)) {
+			const station = req.body.stations.find(
+				(station: Station) => station.name === stationName
+			);
+			if (!station) return res.status(400).send('Wrong staiton names.');
+			const stationSchedule: (Employee | null)[] =
+				req.body.schedules[stationName];
+			if (stationSchedule.length !== station.numberOfCellsInTable)
+				return res.status(400).send('Wrong arrays format.');
+		}
+		next();
 	};
 }
 
-function returnEmployeeRelations(): string[] {
+function returnEmployeeCellNames(): string[] {
 	return [
 		'cell1Employee',
 		'cell2Employee',
@@ -100,4 +167,8 @@ function returnEmployeeRelations(): string[] {
 		'cell11Employee',
 		'cell12Employee',
 	];
+}
+
+function validateDateString(date: string): boolean {
+	return /^\d{4}[-](0?[1-9]|1[012])[-](0?[1-9]|[12][0-9]|3[01])$/.test(date);
 }
--- source/routes/schedulesRouter.ts
+++ source/routes/schedulesRouter.ts
@@ -2,6 +2,15 @@ import express from 'express';
 import { ScheduleController } from '../controllers/schedule/ScheduleController';
 export const schedulesRouter = express.Router();
 
-schedulesRouter.get('/schedule', ScheduleController.getScheduleByDate);
+schedulesRouter.get(
+	'/schedule',
+	ScheduleController.loadStationsAndEmployees,
+	ScheduleController.getScheduleByDate
+);
 
-schedulesRouter.post('/schedule', ScheduleController.saveSchedule);
+schedulesRouter.post(
+	'/schedule',
+	ScheduleController.loadStationsAndEmployees,
+	ScheduleController.reqBodyVeryfier,
+	ScheduleController.saveSchedule
+);
