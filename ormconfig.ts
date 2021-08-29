b766ec8edf444e768c518b3cd110a50d873b70bd
Car <cararr@tlen.pl>
2021-08-29 21:29:12 +0200

    Changed db_user in ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,7 +2,7 @@ module.exports = {
 	type: 'postgres',
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
-	username: process.env.NODE_ENV === 'production' ? 'ubuntu' : 'postgres',
+	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	synchronize: true,
